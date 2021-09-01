9ef5f21f91668847aad83454ac94cf00e3bc9d0b
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2021-08-31 21:48:53 -0300

    Persisting the login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './models/user';
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,23 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private accountService: AccountService) {
 
   }
 
   ngOnInit() {
     this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      const user: User = JSON.parse(storedUser);
+      this.accountService.setCurrentUser(user);
+    } else {
+      this.accountService.setCurrentUser(undefined);
+    }
   }
 
   getUsers() {
